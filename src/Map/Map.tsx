b937d464a5b085ea71a0a18ea6eca57d6b9d9419
Maxwell <maxscaddie@gmail.com>
2020-01-11 15:41:35 +1100

    Made a default floor appear

--- src/Map/Map.tsx
+++ src/Map/Map.tsx
@@ -4,7 +4,7 @@ const Map: React.FC = () => {
     return (
       <React.Fragment>
         <p className="floor">This will say what floor you are looking at</p>
-        <img />
+        <img src="./floors/MB2.jpg" />
       </React.Fragment>);
   }
   
