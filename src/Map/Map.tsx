fe22c5b9d972e228305ac70cd2e284d01ad66238
Unknown <maxscaddie@gmail.com>
2020-01-19 17:00:36 +1100

    Made counter work without redux

--- src/Map/Map.tsx
+++ src/Map/Map.tsx
@@ -2,17 +2,23 @@
 import { store, changeFloor } from "../State";
 
 //Externals
-import React from "react";
+import React, { useState } from "react";
 
 interface IMapProps {
   floor?: string;
 }
 
+function inc(getter: number, setter: React.Dispatch<React.SetStateAction<number>>) {
+  setter(getter + 1);
+}
+
 const Map: React.FC = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <React.Fragment>
-      <button onClick={() => store.dispatch(changeFloor())}>CLICK</button>
-      <p className="floor">This will say what floor you are looking at {store.getState()}</p>
+      <button onClick={() => inc(count, setCount)}>CLICK</button>
+      <p className="floor">This will say what floor you are looking at {count}</p>
       <img src="./floors/MB2.jpg" alt="A floor" />
     </React.Fragment>
   );
