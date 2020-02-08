64596332ca130116fcca1eb1d306c249c47a43b7
Unknown <maxscaddie@gmail.com>
2020-02-08 19:16:05 +1100

    Got CSS to work fully

--- src/Map/Map.tsx
+++ src/Map/Map.tsx
@@ -1,6 +1,7 @@
 //Internals
 import { store } from "../State";
 import { changeFloor } from "../redux/actions/floorActions";
+import "./Map.css";
 
 //Externals
 import React from "react";
@@ -11,7 +12,9 @@ const Map: React.FC = () => {
   const floorOn = useSelector(store.getState);
   return (
     <React.Fragment>
-      <button onClick={() => store.dispatch(changeFloor())}>CLICK</button>
+      <button onClick={() => store.dispatch(changeFloor())} className="button">
+        CLICK
+      </button>
       <p className="floor">{numToFloorName(floorOn)}</p>
       <img src={numToFloor(floorOn)} alt={numToFloor(floorOn)} />
     </React.Fragment>
