0ed5aceae481b1889dff3bb0073fda8f10dfcdc0
Unknown <maxscaddie@gmail.com>
2020-02-28 16:59:00 +1100

    US3.3.2 Put house image in the top bar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,6 +8,9 @@ const Header: React.FC = () => {
   return (
     <div className="header sticky mainColourBackground">
       <img src="./FishStuff/Logo.png" alt="Fishbowls logo" className="logo" />
+      <button onClick={() => alert("Go home you're drunk")} className="home">
+        <img src="./HouseStuff/house.png" alt="Home Button" className="home" />
+      </button>
     </div>
   );
 };
