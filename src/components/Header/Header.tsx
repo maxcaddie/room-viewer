ea43a0f1c229e933a6efb86ca8edb9c84b9ac780
Unknown <maxscaddie@gmail.com>
2020-03-10 22:45:55 +1100

    US3.3.3.0 Spiked router changes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,13 +3,16 @@ import React from "react";
 
 //Stylesheets
 import "./Header.css";
+import { Link } from "react-router-dom";
 
 const Header: React.FC = () => {
   return (
     <div className="header sticky mainColourBackground">
       <img src="./FishStuff/Logo.png" alt="Fishbowls logo" className="logo" />
-      <button onClick={() => alert("Go home you're drunk")} className="home">
+      <button onClick={() => alert("Go home")} className="home">
         <img src="./HouseStuff/house.png" alt="Home Button" className="home" />
+        {/* <Link to="." /> 
+        Router stuff to be added*/}
       </button>
     </div>
   );
